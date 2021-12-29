ba81299eb5d47165a2db6bcdb7fd51db3b251602
Bryan Gurss <btgurss@gmail.com>
2021-12-28 22:03:24 -0600

    Add files via upload

--- Incrementer/incrementer.py
+++ Incrementer/incrementer.py
@@ -1,13 +1,20 @@
 # Take each number of array and add to it its place in the array.  Numbers over 10 will use last digit
 
 def incrementer(num):
+    # Initializing a new list and a staring counter
     new = []
     count=1
+    # Looping through digits in the list
     for digit in num:
+        # Setting temporary variable to digits
         temp = digit + count
+        # Using if statement to change number if greater than 10
         if temp > 9:
+            # Turning number to string and then back to integer after removing last digit
             temp = int(str(temp)[-1])
+        # Counting
         count += 1
+        # Adding temporary number to new list
         new.append(temp)
     return(new)
         
