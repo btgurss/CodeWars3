4915cff1633025bc5e98426d6ace6b4e824aa4e4
Bryan Gurss <btgurss@gmail.com>
2021-12-29 08:43:22 -0600

    Add files via upload

--- SumPairs/SumPairs.py
+++ SumPairs/SumPairs.py
@@ -2,7 +2,7 @@
 # in order of appearance that add up to form the sum.
 
 def sum_pairs(ints, s):
-    temp = [0,0]
+    temp = ['a','b']
     checker = len(ints)
     for i in range(len(ints)-1):
         for j in range(i+1, len(ints)):
@@ -10,9 +10,21 @@ def sum_pairs(ints, s):
                 checker = j
                 temp[0] = ints[i]
                 temp[1] = ints[j]
-    if temp == [0,0]:
-        return "None"
+    if temp == ['a','b']:
+        return 
     return temp
+
+def sum_pairs_efficient(ints, s):
+    # Creating empty set
+    set1 = set()
+    # Looping through numbers in list
+    for num in ints:
+        # Checking if sum subtracted by num is in set
+        if s - num in set1:
+            return [s - num, num]
+        set1.add(num)
+
+
 test = [1, 4, 8, 7, 3, 15]
 test2 = [1, -2, 3, 0, -6, 7]
 print(sum_pairs(test2, 14))
